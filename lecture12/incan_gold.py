049949945de3ae77864217fb1433dbb3b6bc70dc
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-02-15 11:15:35 -0700

    Adding incan gold solution

--- lecture12/incan_gold.py
+++ lecture12/incan_gold.py
@@ -18,6 +18,16 @@ def play_game(n_players: int) -> None:
     Draws a card, then either prints "sorry, better luck next time",
     or displays the number of gems per player and the remainder.
     """
+    card = draw_card()
+    # print(card)
+
+    if card == 0:
+        print("Better luck next time")
+    else:
+        num_per_player = card // n_players
+        remainder = card % n_players
+        print(f"Each player gets {num_per_player} gems "
+            f"with {remainder} left over")
 
 def main() -> None:
     players = int(input("Enter the number of players: "))
