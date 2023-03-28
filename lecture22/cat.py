041746d9f40d2d054f5cc46dcd7dfb425d808f91
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-03-28 16:22:34 -0600

    Adding framework for cat adoption progrma

---
+++ lecture22/cat.py
@@ -0,0 +1,5 @@
+class Cat:
+    def __init__(self) -> None:
+        self.age = 0.5
+        self.colour = "black"
+        self.name = "smokey"
---
+++ lecture22/cat_adoption.py
@@ -0,0 +1,23 @@
+from cat import Cat
+
+def add_cat(cats: list) -> None:
+    """
+    Get info from the user and add to the list.
+    """
+
+def adopt_cat(cats: list, name: str) -> Cat:
+    """
+    Adopt a cat from the list.
+    """
+    # New syntax: pop!
+
+def show_cats(cats: list) -> None:
+    """
+    Print out all the cats in the list.
+    """
+
+def main() -> None:
+    cats = []
+    
+
+main()
