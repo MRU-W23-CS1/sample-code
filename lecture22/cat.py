745bdbd87cc679ec5a4b011600657556540314af
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-03-29 11:20:53 -0600

    Adding cat adoption program for lecture 22

--- lecture22/cat.py
+++ lecture22/cat.py
@@ -1,5 +1,34 @@
+from random import randint
+
 class Cat:
-    def __init__(self) -> None:
-        self.age = 0.5
-        self.colour = "black"
-        self.name = "smokey"
+    def __init__(self, name: str, age: float, colour: str) -> None:
+        self.name = name
+        self.age = age
+        self.colour = colour
+        self.adopted = False
+    
+    def __str__(self) -> str:
+        """
+        Called when cat object is printed.
+        """
+        desc = f"Cat with name {self.name}"
+        if self.adopted:
+            desc += " has been adopted!"
+        else:
+            desc += " is available for adoption."
+        return desc
+
+    def scratch(self, thing: str) -> int:
+        """
+        Cat scratches the thing. Returns amount of damage done.
+        """
+        print(f"{self.name} scratches the {thing}!")
+        return randint(0, 10)
+
+if __name__ == '__main__':
+    # name = input("What is the cat's name? ")
+    test_cat = Cat("Salvador", 5, "Tuxedo")
+    print(f"Created a cat object: {test_cat}")
+    c_damage = test_cat.scratch("couch")
+    d_damage = test_cat.scratch("Dog")
+    print(f"{test_cat.name} did a total of {c_damage + d_damage} damage")
--- lecture22/cat_adoption.py
+++ lecture22/cat_adoption.py
@@ -4,20 +4,48 @@ def add_cat(cats: list) -> None:
     """
     Get info from the user and add to the list.
     """
+    name = input("Enter the cat's name: ")
+    # might want some input validation here
+    age = float(input("Enter the cat's age: "))
+    colour = input("Enter the cat's colour: ")
+    new_cat = Cat(name, age, colour)
+    cats.append(new_cat)
 
 def adopt_cat(cats: list, name: str) -> Cat:
     """
     Adopt a cat from the list.
     """
-    # New syntax: pop!
+    adopted = None
+    for i in range(len(cats)):
+        if cats[i].name == name:
+            cats[i].adopted = True
+            adopted = cats[i]
+    
+    return adopted
 
 def show_cats(cats: list) -> None:
     """
     Print out all the cats in the list.
     """
+    print("Here are all the cats:")
+    for cat in cats:
+        print(cat)
 
 def main() -> None:
     cats = []
-    
+    keep_adding = input("Would you like to add a cat? y/n: ")
+    while keep_adding == "y":
+        add_cat(cats)
+        keep_adding = input("Add another cat? y/n: ")
+
+    show_cats(cats)
+
+    to_adopt = input("Which cat would you like to adopt? ")
+    take_home = adopt_cat(cats, to_adopt)
+
+    show_cats(cats)
+
+    if take_home is not None:
+        print(f"We're taking home {take_home.name}.")
 
 main()
